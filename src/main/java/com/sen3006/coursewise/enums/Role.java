74b99603d6e29b5088c03fe128b6a0d6373e0ea4
ToxicCicity <ksforunity@gmail.com>
2025-04-23 09:49:53 +0300

    prepared enums

---
+++ src/main/java/com/sen3006/coursewise/enums/Role.java
@@ -0,0 +1,20 @@
+package com.sen3006.coursewise.enums;
+
+public enum Role {
+    STUDENT,
+    ADMIN,
+    ACADEMIC;
+
+    public int role(Role role) {
+        switch (role) {
+            case STUDENT:
+                return 0;
+            case ADMIN:
+                return 1;
+            case ACADEMIC:
+                return 2;
+            default:
+                return -1;
+        }
+    }
+}
