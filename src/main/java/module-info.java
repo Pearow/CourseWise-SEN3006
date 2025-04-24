4eebac69d534330da32f59d41ee3212e10587ff2
Pearow <salimmert2003@hotmail.com>
2025-04-24 18:58:54 +0300

    Install gson for json handling

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.sen3006.coursewise {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
-    opens com.sen3006.coursewise to javafx.fxml;
+    opens com.sen3006.coursewise to javafx.fxml, com.google.gson;
     exports com.sen3006.coursewise;
 }
